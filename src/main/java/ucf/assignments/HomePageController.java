2122eb761494a5fe60bf8b61a47169998fe85bcf
Alice Yu <alicesyu2002@gmail.com>
2021-07-05 06:39:34 -0400

    Initial commit

---
+++ src/main/java/ucf/assignments/HomePageController.java
@@ -0,0 +1,21 @@
+/*
+ *
+ *  *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  *  Copyright 2021 Alice Yu
+ *
+ */
+
+package ucf.assignments;
+
+import javafx.event.ActionEvent;
+
+public class HomePageController {
+    public void addListClicked(ActionEvent actionEvent) {
+    }
+
+    public void removeListClicked(ActionEvent actionEvent) {
+    }
+
+    public void editListClicked(ActionEvent actionEvent) {
+    }
+}
---
+++ src/test/java/ucf/assignments/HomePageControllerTest.java
@@ -0,0 +1,27 @@
+/*
+ *
+ *  *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  *  Copyright 2021 Alice Yu
+ *
+ */
+
+package ucf.assignments;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HomePageControllerTest {
+
+    @Test
+    void addListClicked() {
+    }
+
+    @Test
+    void removeListClicked() {
+    }
+
+    @Test
+    void editListClicked() {
+    }
+}
